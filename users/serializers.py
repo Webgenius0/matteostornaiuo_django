4157a2e3974c5daff9f1750a7e40dff82040e9ba
badsha-ugv <badsha.ugv>
2025-05-28 11:54:28 +0600

    invitation code allow null true

--- users/serializers.py
+++ users/serializers.py
@@ -35,7 +35,7 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class StaffSignupSerializer(serializers.ModelSerializer):
-    client_invitation_code = serializers.CharField(max_length=100, write_only=True, required=False)
+    client_invitation_code = serializers.CharField(max_length=100, write_only=True, required=False, allow_null=True)
 
     class Meta:
         model = User

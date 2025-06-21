6b5811f121658792597a99068c34ed0a452b046c
badsha-ugv <badsha.ugv@gmail.com>
2025-06-21 12:32:29 +0600

    up asgi

--- project/asgi.py
+++ project/asgi.py
@@ -6,21 +6,19 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
-
 import os
-
-from channels.routing import ProtocolTypeRouter, URLRouter
-from dashboard.middleware import JWTAuthMiddlewareStack
-from dashboard.routing import websocket_urlpatterns
-from chat.routing import websocket_urlpatterns as chat_websocket_urlpatterns
-from channels.auth import AuthMiddlewareStack
-
-
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
+# Set up Django first
+django_asgi_app = get_asgi_application()
 
+# Now safe to import channels and routing
+from channels.routing import ProtocolTypeRouter, URLRouter
+from dashboard.middleware import JWTAuthMiddlewareStack
+from dashboard.routing import websocket_urlpatterns
+from chat.routing import websocket_urlpatterns as chat_websocket_urlpatterns
 
 # application = get_asgi_application()
 
